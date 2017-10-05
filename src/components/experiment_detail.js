905a7b0415077e59d6966af4f428d633b1f0d0b8
Teon L Brooks <teon.brooks@gmail.com>
2017-10-04 18:22:03 -0700

    wip with fetch

--- src/components/experiment_detail.js
+++ src/components/experiment_detail.js
@@ -6,10 +6,19 @@ const ExperimentDetail = ({experiment}) => {
     return <div>Loading...</div>;
   }
 
+  fetch(experiment.url).then( request => {
+    return request.json()
+  }).then( json => {
+    console.log('request', json);
+    console.log('path', json['download_url']);
+  });
+
   return (
     <div className="experiment-details col-md-8">
       <div>Experiment Name: {experiment.repository.full_name}</div>
-      <div>Url: <a href={experiment.html_url}>File</a></div>
+      <div>Url: <a href={experiment.url}>File</a></div>
+
+      <div></div>
     </div>
   );
 };
